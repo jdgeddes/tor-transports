13362ab88f10609c22ff675e870e9bfdee91f433
John Geddes <geddes@cs.umn.edu>
2015-10-09 11:46:53 -0500

    updated run tests script

--- xtcp/run-tests.py
+++ xtcp/run-tests.py
@@ -7,6 +7,7 @@ import subprocess
 import time
 
 libs = ['template', 'utp']
+tests = ['filexfer', 'openssl', 'tgen']
 
 def main():
     if len(sys.argv) == 1 or sys.argv[1].lower() not in libs:
@@ -17,10 +18,14 @@ def main():
     library = sys.argv[1].lower()
 
     env = dict(os.environ)
-    env['LD_LIBRARY_PATH'] += ':' + os.getcwd()
+    if 'LD_LIBRARY_PATH' in env:
+        env['LD_LIBRARY_PATH'] += ':' + os.getcwd()
+    else:
+        env['LD_LIBRARY_PATH'] = os.getcwd()
     env['LD_PRELOAD'] = os.path.join(os.getcwd(), 'libxtcp_{0}.so'.format(library))
 
-    for testdir in os.listdir('tests'):
+    # for testdir in os.listdir('tests'):
+    for testdir in tests:
         print testdir
         parentdir = os.path.join('tests', testdir)
         servercmd = './run-server.sh'
@@ -36,15 +41,15 @@ def main():
 
         serverout = open(os.path.join(parentdir, 'server-out.log'), 'w')
         servererr = open(os.path.join(parentdir, 'server-err.log'), 'w')
-        env['XTCP_LOG'] = 'server-xtcp.log'
-        serverproc = subprocess.Popen(servercmd, shell=True, stdout=serverout, stderr=servererr, env=env, cwd=parentdir)
+        # env['XTCP_LOG'] = 'server-xtcp.log'
+        serverproc = subprocess.Popen(servercmd, shell=True, stdout=serverout, stderr=serverout, env=env, cwd=parentdir)
 
         time.sleep(1)
 
         clientout = open(os.path.join(parentdir, 'client-out.log'), 'w')
         clienterr = open(os.path.join(parentdir, 'client-err.log'), 'w')
-        env['XTCP_LOG'] = 'client-xtcp.log'
-        clientproc = subprocess.Popen(clientcmd, shell=True, stdout=clientout, stderr=clienterr, env=env, cwd=parentdir)
+        # env['XTCP_LOG'] = 'client-xtcp.log'
+        clientproc = subprocess.Popen(clientcmd, shell=True, stdout=clientout, stderr=clientout, env=env, cwd=parentdir)
 
         clientproc.wait()
 
